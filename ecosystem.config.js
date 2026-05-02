c76451e0bb123d444d57bdc9ddfa2028e7ef7f61
Anushanga <anushangasharada@gmail.com>
2026-05-02 18:51:27 +0530

    chore: increase memory limits for backend process in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'wagex-backend',
       script: './dist/main.js',
-      node_args: '--max-old-space-size=384',
-      max_memory_restart: '450M',
+      node_args: '--max-old-space-size=640',
+      max_memory_restart: '750M',
       error_file: './logs/error.log',
       out_file: './logs/out.log',
       time: true,
